984aaeb4b5f78def07964c2ca845436c47ebba4d
konsy <785823856@qq.com>
2021-12-11 01:34:17 +0800

    #1 comment 注释

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const AutoImport = require('unplugin-auto-import/webpack')
 const Components = require('unplugin-vue-components/webpack')
 const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
-
+// 自动导入
+// First you need install unplugin-vue-components and unplugin-auto-import.
+// npm install -D unplugin-vue-components unplugin-auto-import
 module.exports = {
     // ...
     plugins: [
